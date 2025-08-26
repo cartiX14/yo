825b0ce8791dbbc9a2206c6ff75aa3a2cdccb5be
Osama <164055961+cartiX14@users.noreply.github.com>
2025-08-26 13:52:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const yearSpan = document.getElementById('year');
-if (yearSpan) {
-  yearSpan.textContent = new Date().getFullYear();
-}
 
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener('click', e => {
-    e.preventDefault();
-    const target = document.querySelector(link.getAttribute('href'));
-    if (target) target.scrollIntoView({ behavior: 'smooth' });
-  });
-});
